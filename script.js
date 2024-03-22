aefa249d705b23a958b6a8a6a7b3c285d92276b2
Andrei Scafi <andreiscafi@hotmail.com>
2024-03-22 13:24:33 -0300

    Adding check Message Button event listener with javascript.

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const messageInput = document.getElementById('message-input');
 const result = document.getElementById('result');
 const checkMessageButton = document.getElementById('check-message-btn');
 //End of variables
+//Eventlistener
+checkMessageButton.addEventListener('click', () => {
+    if (messageInput.value === "") {
+        alert("Please enter a message.");
+        return;
+    }
+});
+//End of Eventlistener
