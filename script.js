8b1a14cedd7879c4938c4afe9adda3a009bcfd42
Andrei Scafi <andreiscafi@hotmail.com>
2024-03-23 17:54:53 -0300

    Adding isSpam function and updating adventListener with javascript.

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ const messageInput = document.getElementById('message-input');
 const result = document.getElementById('result');
 const checkMessageButton = document.getElementById('check-message-btn');
 //End of variables
+
+//functions
+
+const isSpam = (msg) => false;
+
+//End of functions
+
 //Eventlistener
 checkMessageButton.addEventListener('click', () => {
     if (messageInput.value === "") {
         alert("Please enter a message.");
         return;
     }
+
+    result.textContent = isSpam(messageInput.value)
+        ? "Oh no! This looks like a spam message."
+        : "This message does not seem to contain any spam.";
+    messageInput.value = "";
 });
 //End of Eventlistener
