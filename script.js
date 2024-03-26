961527248b101493344cf103ea88b26e007949f4
Andrei Scafi <andreiscafi@hotmail.com>
2024-03-26 16:50:27 -0300

    Updating denyList with dollarRegex expression.

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const result = document.getElementById('result');
 const checkMessageButton = document.getElementById('check-message-btn');
 
 const helpRegex = /please help|assist me/i;
+const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
 
-const denyList = [helpRegex];
+const denyList = [helpRegex, dollarRegex];
 //End of variables
 
 //functions
